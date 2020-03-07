6dbe136090344d359670e809b5fbf6d3ef50cd69
Anurag-Yoda <anurag.404@outlook.com>
2020-03-07 21:54:45 +0530

    added form submit in local state

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 
 const eventsFromDash = [
@@ -57,15 +58,36 @@ const eventsFromDash = [
 
 
 class EventDashboard extends Component {
+    state = {
+        events : eventsFromDash,
+        isOpen: false
+    }
+
+    createEventHandler = () => {
+      this.setState((prevState)=>(
+          {isOpen : !prevState.isOpen}
+      ));
+    };
+
+    handleCreateEvent = (newEvent) => {
+        newEvent.id = cuid();
+        newEvent.hostPhotoURL = '/assets/user.png';
+        this.setState(({events})=>({
+         events:[...events, newEvent], isOpen: false  
+        }));
+      
+    }
     render() {
+        const {events, isOpen} = this.state;
         return (
           <Grid>
               <Grid.Column width = {10}>
-                  <EventList events = {eventsFromDash}/>
+                  <EventList events = {events}/>
               </Grid.Column>
               <Grid.Column width = {6}>
-              <Button positive content ='Create Event'/>
-                  <EventForm/>
+              <Button positive content ='Create Event' onClick = {this.createEventHandler}/>
+              {isOpen ?  <EventForm cancelFormOpen = {this.createEventHandler} eventCreate = {this.handleCreateEvent}/> : null }
+                 
               </Grid.Column>
           </Grid>
         )
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,35 +1,55 @@
 import React, { Component } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react';
 
+ const formInput = {};
+
  class EventForm extends Component {
+     state = {
+         title : ''
+     }
+     formInput = this.state;
+
+
+     handleTitleChange = event => {
+         this.setState({[event.target.name] : event.target.value});
+         
+     }
+
+     handleFormSubmit = (event) => {
+         event.preventDefault();
+         this.props.eventCreate(this.state);
+        }  
+
     render() {
-        return (
+        const {title} = this.state;
+        const {cancelFormOpen} = this.props;
+         return (
                   <Segment>
-                    <Form>
+                    <Form autoComplete = 'off' onSubmit = {this.handleFormSubmit}>
                       <Form.Field>
                         <label>Event Title</label>
-                        <input placeholder="First Name" />
+                        <input placeholder="First Name" name = 'title' onChange ={ this.handleTitleChange}/>
                       </Form.Field>
                       <Form.Field>
                         <label>Event Date</label>
-                        <input type="date" placeholder="Event Date" />
+                        <input type="date" placeholder="Event Date" name = 'Date' onChange ={ this.handleTitleChange}/>
                       </Form.Field>
                       <Form.Field>
                         <label>City</label>
-                        <input placeholder="City event is taking place" />
+                        <input placeholder="City event is taking place" name = 'city' onChange ={ this.handleTitleChange} />
                       </Form.Field>
                       <Form.Field>
                         <label>Venue</label>
-                        <input placeholder="Enter the Venue of the event" />
+                        <input placeholder="Enter the Venue of the event" name = 'venue' onChange ={ this.handleTitleChange}/>
                       </Form.Field>
                       <Form.Field>
                         <label>Hosted By</label>
-                        <input placeholder="Enter the name of person hosting" />
+                        <input placeholder="Enter the name of person hosting" name = 'hostedby' onChange ={ this.handleTitleChange}/>
                       </Form.Field>
-                      <Button positive type="submit">
+                      <Button positive type="submit" >
                         Submit
                       </Button>
-                      <Button type="button">Cancel</Button>
+                      <Button type="button" onClick ={cancelFormOpen}>Cancel</Button>
                     </Form>
                   </Segment>
         )
